9494ce79a042db876b1bad1faa5c8c565b40fe45
Yihe Li <winmikedows@hotmail.com>
2026-04-27 05:41:31 +0800

    Add basic tests

--- tests/beman/range_searcher/basic.test.cpp
+++ tests/beman/range_searcher/basic.test.cpp
@@ -8,15 +8,8 @@
 
 #include <beman/range_searcher/searcher.hpp>
 
-namespace exe = beman::scan_view;
-
-template <class E1, class E2, std::size_t N, class Join = std::plus<E1>>
-auto joinArrays(E1 (&a)[N], E2 (&b)[N], Join join = Join()) {
-    return exe::scan(a, [&a, &b, join](auto sum, auto& cur) {
-        auto idx = (&cur) - a;
-        return join(sum, b[idx]);
-    });
-}
+namespace exe     = beman::range_searcher;
+namespace branges = exe::ranges;
 
 struct NonConstView : std::ranges::view_base {
     explicit NonConstView(int* b, int* e) : b_(b), e_(e) {}
@@ -24,11 +17,6 @@ struct NonConstView : std::ranges::view_base {
     const int* end() { return e_; }   // deliberately non-const
     const int* b_;
     const int* e_;
-};
-
-struct NonTrivialFunctor {
-    int           var_;
-    constexpr int operator()(int a, int b) const { return a + b; }
 };
 
 struct MoveOnlyFunctor {
@@ -40,135 +28,69 @@ struct MoveOnlyFunctor {
     int              operator()(int a, const std::unique_ptr<int>& b) const { return a + *b; }
 };
 
-struct MoveOnlyNonTrivialFunctor : MoveOnlyFunctor {
-    int var_;
-};
-
-// Some basic examples of how transform_view might be used in the wild. This is a general
-// collection of sample algorithms and functions that try to mock general usage of
-// this view.
-
 TEST(RangeSearcher, General) {
-    std::vector<int> vec         = {1, 2, 3, 4};
-    auto             transformed = exe::scan(vec, std::plus{});
-    int              expected[]  = {1, 3, 6, 10};
-    ASSERT_TRUE(std::ranges::equal(transformed, expected));
-    const auto& ct = transformed;
-    ASSERT_TRUE(std::ranges::equal(ct, expected));
+    std::vector vec  = {1, 2, 3, 4};
+    std::vector vec2 = {2, 3};
+    std::vector vec3 = {2, 4};
+    ASSERT_TRUE(branges::contains_subrange(vec, branges::default_searcher{vec2}));
+    ASSERT_EQ(branges::search(vec, branges::default_searcher{vec2}).begin() - vec.begin(), 1);
+    ASSERT_EQ(branges::search(vec, branges::default_searcher{vec2}).end() - vec.begin(), 3);
+    ASSERT_FALSE(branges::contains_subrange(vec, branges::default_searcher{vec3}));
+    ASSERT_EQ(branges::search(vec, branges::default_searcher{vec3}).begin(), vec.end());
+    ASSERT_EQ(branges::search(vec, branges::default_searcher{vec3}).end(), vec.end());
+    ASSERT_TRUE(branges::contains_subrange(vec, branges::boyer_moore_searcher{vec2}));
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_searcher{vec2}).begin() - vec.begin(), 1);
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_searcher{vec2}).end() - vec.begin(), 3);
+    ASSERT_FALSE(branges::contains_subrange(vec, branges::boyer_moore_searcher{vec3}));
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_searcher{vec3}).begin(), vec.end());
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_searcher{vec3}).end(), vec.end());
+    ASSERT_TRUE(branges::contains_subrange(vec, branges::boyer_moore_horspool_searcher{vec2}));
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_horspool_searcher{vec2}).begin() - vec.begin(), 1);
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_horspool_searcher{vec2}).end() - vec.begin(), 3);
+    ASSERT_FALSE(branges::contains_subrange(vec, branges::boyer_moore_horspool_searcher{vec3}));
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_horspool_searcher{vec3}).begin(), vec.end());
+    ASSERT_EQ(branges::search(vec, branges::boyer_moore_horspool_searcher{vec3}).end(), vec.end());
 
-    auto transformed2 = exe::scan(vec, std::plus{}, 10);
-    int  expected2[]  = {11, 13, 16, 20};
-    ASSERT_TRUE(std::ranges::equal(transformed2, expected2));
-    const auto& ct2 = transformed2;
-    ASSERT_TRUE(std::ranges::equal(ct2, expected2));
+    std::string      haystack = "a quick brown fox jumps over the lazy dog";
+    std::string_view needle   = "jump";
+    ASSERT_TRUE(branges::contains_subrange(haystack, branges::default_searcher{needle}));
+    ASSERT_EQ(branges::search(haystack, branges::default_searcher{needle}).begin() - haystack.begin(), 18);
+    ASSERT_EQ(branges::search(haystack, branges::default_searcher{needle}).end() - haystack.begin(), 22);
+    ASSERT_FALSE(branges::contains_subrange(haystack, branges::default_searcher{"Jump"}));
+    ASSERT_EQ(branges::search(haystack, branges::default_searcher{"Jump"}).begin(), haystack.end());
+    ASSERT_EQ(branges::search(haystack, branges::default_searcher{"Jump"}).end(), haystack.end());
+    ASSERT_TRUE(branges::contains_subrange(haystack, branges::boyer_moore_searcher{needle}));
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_searcher{needle}).begin() - haystack.begin(), 18);
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_searcher{needle}).end() - haystack.begin(), 22);
+    ASSERT_FALSE(branges::contains_subrange(haystack, branges::boyer_moore_searcher{"Jump"}));
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_searcher{"Jump"}).begin(), haystack.end());
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_searcher{"Jump"}).end(), haystack.end());
+    ASSERT_TRUE(branges::contains_subrange(haystack, branges::boyer_moore_horspool_searcher{needle}));
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_horspool_searcher{needle}).begin() - haystack.begin(),
+              18);
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_horspool_searcher{needle}).end() - haystack.begin(), 22);
+    ASSERT_FALSE(branges::contains_subrange(haystack, branges::boyer_moore_horspool_searcher{"Jump"}));
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_horspool_searcher{"Jump"}).begin(), haystack.end());
+    ASSERT_EQ(branges::search(haystack, branges::boyer_moore_horspool_searcher{"Jump"}).end(), haystack.end());
 }
 
 TEST(RangeSearcher, Constexpr) {
-    static constexpr std::array<int, 4> arr         = {1, 2, 3, 4};
-    static constexpr auto               transformed = exe::scan(arr, std::plus{});
-    static constexpr std::array<int, 4> expected    = {1, 3, 6, 10};
-    static_assert(std::ranges::equal(transformed, expected));
+    static constexpr std::array vec  = {1, 2, 3, 4};
+    static constexpr std::array vec2 = {2, 3};
+    static constexpr std::array vec3 = {2, 4};
+    static_assert(branges::contains_subrange(vec, branges::default_searcher{vec2}));
+    static_assert(branges::search(vec, branges::default_searcher{vec2}).begin() == vec.begin() + 1);
+    static_assert(branges::search(vec, branges::default_searcher{vec2}).end() == vec.begin() + 3);
+    static_assert(!branges::contains_subrange(vec, branges::default_searcher{vec3}));
+    static_assert(branges::search(vec, branges::default_searcher{vec3}).begin() == vec.end());
+    static_assert(branges::search(vec, branges::default_searcher{vec3}).end() == vec.end());
 
-    static constexpr auto               transformed2 = exe::scan(arr, std::plus{}, 10);
-    static constexpr std::array<int, 4> expected2    = {11, 13, 16, 20};
-    static_assert(std::ranges::equal(transformed2, expected2));
+    static constexpr std::string_view haystack = "a quick brown fox jumps over the lazy dog";
+    static constexpr std::string_view needle   = "jump";
+    static_assert(branges::contains_subrange(haystack, branges::default_searcher{needle}));
+    static_assert(branges::search(haystack, branges::default_searcher{needle}).begin() == haystack.begin() + 18);
+    static_assert(branges::search(haystack, branges::default_searcher{needle}).end() == haystack.begin() + 22);
+    static_assert(!branges::contains_subrange(haystack, branges::default_searcher{"Jump"}));
+    static_assert(branges::search(haystack, branges::default_searcher{"Jump"}).begin() == haystack.end());
+    static_assert(branges::search(haystack, branges::default_searcher{"Jump"}).end() == haystack.end());
 }
-
-TEST(RangeSearcher, Properties) {
-    std::vector<int> vec         = {1, 2, 3, 4};
-    auto             transformed = exe::scan(vec, std::plus{});
-    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(transformed)>, int>);
-    static_assert(std::is_same_v<std::ranges::range_reference_t<decltype(transformed)>, const int&>);
-    static_assert(std::ranges::input_range<decltype(transformed)> &&
-                  !std::ranges::forward_range<decltype(transformed)>);
-    static_assert(!std::ranges::common_range<decltype(transformed)>);
-    static_assert(std::ranges::sized_range<decltype(transformed)>);
-    static_assert(std::ranges::range<const decltype(transformed)>);
-    static_assert(std::ranges::borrowed_range<decltype(transformed)>);
-    ASSERT_EQ(transformed.size(), 4);
-
-    auto transformed2 = exe::scan(vec, std::plus{}, 10);
-    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(transformed2)>, int>);
-    static_assert(std::is_same_v<std::ranges::range_reference_t<decltype(transformed2)>, const int&>);
-    static_assert(std::ranges::input_range<decltype(transformed2)> &&
-                  !std::ranges::forward_range<decltype(transformed2)>);
-    static_assert(!std::ranges::common_range<decltype(transformed2)>);
-    static_assert(std::ranges::sized_range<decltype(transformed2)>);
-    static_assert(std::ranges::range<const decltype(transformed2)>);
-    static_assert(std::ranges::borrowed_range<decltype(transformed2)>);
-    ASSERT_EQ(transformed2.size(), 4);
-
-    auto transformed3 = exe::scan(vec, NonTrivialFunctor{});
-    static_assert(std::is_same_v<std::ranges::range_value_t<decltype(transformed3)>, int>);
-    static_assert(std::is_same_v<std::ranges::range_reference_t<decltype(transformed3)>, const int&>);
-    static_assert(std::ranges::input_range<decltype(transformed3)> &&
-                  !std::ranges::forward_range<decltype(transformed3)>);
-    static_assert(!std::ranges::common_range<decltype(transformed3)>);
-    static_assert(std::ranges::sized_range<decltype(transformed3)>);
-    static_assert(std::ranges::range<const decltype(transformed3)>);
-    static_assert(!std::ranges::borrowed_range<decltype(transformed3)>);
-    ASSERT_EQ(transformed3.size(), 4);
-}
-
-TEST(RangeSearcher, NonConstIterable) {
-    // Test a view type that is not const-iterable.
-    int  a[]         = {1, 2, 3, 4};
-    auto transformed = NonConstView(a, a + 4) | exe::scan(std::plus{});
-    int  expected[]  = {1, 3, 6, 10};
-    ASSERT_TRUE(std::ranges::equal(transformed, expected));
-
-    auto transformed2 = NonConstView(a, a + 4) | exe::scan(std::plus{}, 10);
-    int  expected2[]  = {11, 13, 16, 20};
-    ASSERT_TRUE(std::ranges::equal(transformed2, expected2));
-}
-
-TEST(RangeSearcher, Borrowed) {
-    std::vector<int>                              vec = {1, 2, 3, 4};
-    decltype(exe::scan(vec, std::plus{}).begin()) it;
-    {
-        auto transformed = exe::scan(vec, std::plus{});
-        it               = transformed.begin();
-    }
-    ASSERT_EQ(*it, 1);
-    ++it;
-    ASSERT_EQ(*it, 3);
-}
-
-TEST(RangeSearcher, JoinArray) {
-    int  a[]     = {1, 2, 3, 4};
-    int  b[]     = {4, 3, 2, 1};
-    auto out     = joinArrays(a, b);
-    int  check[] = {1, 4, 6, 7};
-    ASSERT_TRUE(std::ranges::equal(out, check));
-
-    auto out2     = joinArrays(a, b, std::multiplies{});
-    int  check2[] = {1, 3, 6, 6};
-    ASSERT_TRUE(std::ranges::equal(out2, check2));
-}
-
-#if __cplusplus >= 202302L && __cpp_lib_ranges >= 202207L // FTM for P2494R2
-TEST(RangeSearcher, MoveOnly) {
-    std::vector<std::unique_ptr<int>> vec4;
-    vec4.push_back(std::make_unique<int>(5));
-    vec4.push_back(std::make_unique<int>(2));
-    vec4.push_back(std::make_unique<int>(10));
-    auto out     = exe::scan(vec4, [](const auto& a, const auto& b) { return a + *b; }, 3);
-    int  check[] = {8, 10, 20};
-    ASSERT_TRUE(std::ranges::equal(out, check));
-
-    decltype(exe::scan(vec4, MoveOnlyFunctor{}, 3).begin()) it;
-    {
-        auto out2 = exe::scan(vec4, MoveOnlyFunctor{}, 3);
-        static_assert(std::ranges::borrowed_range<decltype(out2)>);
-        ASSERT_TRUE(std::ranges::equal(out2, check));
-        it = out2.begin();
-    }
-    ASSERT_EQ(*it, 8);
-    ++it;
-    ASSERT_EQ(*it, 10);
-
-    auto out3 = exe::scan(vec4, MoveOnlyNonTrivialFunctor{}, 3);
-    static_assert(!std::ranges::borrowed_range<decltype(out3)>);
-    ASSERT_TRUE(std::ranges::equal(out3, check));
-}
-#endif
